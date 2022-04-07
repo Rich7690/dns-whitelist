99db3b197f65cacddd0715f450fe3647f1d1fdff
Rich7690 <11451714+Rich7690@users.noreply.github.com>
2022-04-06 21:11:26 -0700

    add cloudflare

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module rtdev.nl/dnswhitelist
 
 go 1.17
 
-require github.com/oracle/oci-go-sdk/v50 v50.0.0
+require (
+	github.com/cloudflare/cloudflare-go v0.35.1
+	github.com/oracle/oci-go-sdk/v50 v50.0.0
+)
 
 require (
 	code.gitea.io/sdk/gitea v0.13.2 // indirect
@@ -11,10 +14,13 @@ require (
 	github.com/google/go-github/v30 v30.1.0 // indirect
 	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/hashicorp/go-version v1.2.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/ulikunitz/xz v0.5.8 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
 	golang.org/x/oauth2 v0.0.0-20201109201403-9fd604954f58 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net"
 	"os"
+	"strings"
 
+	"github.com/cloudflare/cloudflare-go"
 	"github.com/creativeprojects/go-selfupdate"
 	"github.com/oracle/oci-go-sdk/v50/common"
 	"github.com/oracle/oci-go-sdk/v50/common/auth"
@@ -15,8 +18,12 @@ import (
 var (
 	version         string
 	commit          string
-	DisableUpdate   bool = os.Getenv("DISABLE_UPDATE") == "true"
-	DisableChecking bool = os.Getenv("DISABLE_CHECKS") == "true"
+	DisableUpdate   bool   = os.Getenv("DISABLE_UPDATE") == "true"
+	DisableChecking bool   = os.Getenv("DISABLE_CHECKS") == "true"
+	provider        string = os.Getenv("PROVIDER")
+	dnsRecord       string = os.Getenv("DNS_RECORD")
+	listID          string = os.Getenv("LIST_ID")
+	accountID       string = os.Getenv("ACCOUNT_ID")
 )
 
 func doSelfUpdate() {
@@ -31,9 +38,9 @@ func doSelfUpdate() {
 	}
 
 	if DisableUpdate {
-		latest, found, err := updater.DetectLatest("rtdev7690/dns-whitelist")
-		if err != nil {
-			log.Printf("Error finding latest version: %v\n", err)
+		latest, found, lerr := updater.DetectLatest("rtdev7690/dns-whitelist")
+		if lerr != nil {
+			log.Printf("Error finding latest version: %v\n", lerr)
 			return
 		}
 		if found {
@@ -62,19 +69,96 @@ func doSelfUpdate() {
 	}
 }
 
-func main() {
-	log.Println("Version: " + version)
-	log.Println("Commit: " + commit)
-	ctx, cancel := context.WithCancel(context.Background())
-	doSelfUpdate()
+// O(N) contains check for cloudflare ip slices. Use sets or maps to optimize when possible
+func contains(elems []cloudflare.IPListItem, v string) bool {
+	for i := range elems {
+		if elems[i].IP == v {
+			return true
+		}
+	}
+	return false
+}
 
-	addrs, err := net.LookupHost(os.Getenv("DNS_RECORD"))
+// O(N) contains check for strings. Use sets or maps to optimize when possible
+func containsString(elems []string, v string) bool {
+	for i := range elems {
+		if elems[i] == v {
+			return true
+		}
+	}
+	return false
+}
+
+func whitelistCloudflare(ctx context.Context) error {
+	api, err := cloudflare.NewWithAPIToken(os.Getenv("CLOUDFLARE_API_TOKEN"), cloudflare.UsingAccount(accountID))
 	if err != nil {
-		log.Fatal(err)
+		return err
+	}
+
+	list, err := api.ListIPListItems(ctx, listID)
+	if err != nil {
+		return err
+	}
+
+	records := strings.Split(dnsRecord, ";")
+
+	if len(records) == 0 {
+		return errors.New("no dns records set to resolve")
+	}
+
+	var ips = make([]string, 0)
+
+	for i := range records {
+		addrs, err := net.LookupHost(records[i])
+		if err != nil {
+			return err
+		}
+		ips = append(ips, addrs...)
+	}
+	log.Println("desired ips: ", ips)
+
+	var toAdd []cloudflare.IPListItemCreateRequest
+	var toDelete []cloudflare.IPListItemDeleteItemRequest
+
+	for i := range ips {
+		// if the desired ip isn't in the existing list add it
+		if !contains(list, ips[i]) {
+			toAdd = append(toAdd, cloudflare.IPListItemCreateRequest{IP: ips[i]})
+		}
+	}
+	for i := range list {
+		// if an item exists that isnt in the desired list, delete it.
+		if !containsString(ips, list[i].IP) {
+			toDelete = append(toDelete, cloudflare.IPListItemDeleteItemRequest{ID: list[i].ID})
+		}
+	}
+
+	log.Println("toAdd: ", toAdd, " toDelete: ", toDelete)
+
+	if len(toAdd) != 0 {
+		_, err := api.CreateIPListItems(ctx, listID, toAdd)
+		if err != nil {
+			return err
+		}
+	}
+	if len(toDelete) != 0 {
+		_, err := api.DeleteIPListItems(ctx, listID, cloudflare.IPListItemDeleteRequest{Items: toDelete})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func whitelistOCI(ctx context.Context) error {
+	addrs, err := net.LookupHost(dnsRecord)
+	if err != nil {
+		return err
 	}
 
 	if len(addrs) == 0 {
-		log.Fatal("Failed to resolve addresses")
+		return errors.New("failed to resolve addresses")
 	}
 
 	log.Printf("%v", addrs)
@@ -84,14 +168,14 @@ func main() {
 	if os.Getenv("LOCAL") == "" {
 		provider, err = auth.InstancePrincipalConfigurationProvider()
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 	}
 
 	oci, err := core.NewVirtualNetworkClientWithConfigurationProvider(provider)
 
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	policy := common.DefaultRetryPolicy()
@@ -102,7 +186,7 @@ func main() {
 	})
 
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	portMap := map[int]bool{80: true, 443: true, 7000: true}
@@ -133,6 +217,24 @@ func main() {
 			SecurityRules: updatedRules,
 		},
 	})
+	return err
+}
+
+func main() {
+	log.Println("Version: " + version)
+	log.Println("Commit: " + commit)
+	ctx, cancel := context.WithCancel(context.Background())
+	doSelfUpdate()
+
+	var err error
+	switch provider {
+	case "OCI":
+		err = whitelistOCI(ctx)
+	case "Cloudflare":
+		err = whitelistCloudflare(ctx)
+	default:
+		err = errors.New("unsupported provider: " + provider)
+	}
 
 	if err != nil {
 		log.Fatal(err)
